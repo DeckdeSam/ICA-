47a4e003f5ed1bdb6ed27ddd43c1bf53a2b3c9da
DeckdeSam <crsytpubg@gmail.com>
2025-10-06 09:03:39 +0530

    Sam

---
+++ JAVA/DataTypes.java
@@ -0,0 +1,36 @@
+class DataTypes 
+{
+	public static void main(String[] args) 
+	{
+		// identifire in java
+        int a = 10; // a is idenfire and int is a data types
+		int b = 20; // b is idenfire and int is a data types
+		int c = 30; // c is idenfire and int is a data types
+		System.out.println();
+		
+		
+		// integer dataTypes
+		byte a = 10;
+		short c = 10;
+		int d = 10; // int is a interger data types dont allow for decimal values
+		long b 55l;
+		
+		// decimal values
+		float a = 78.54;
+		double a = 69.45d;
+		
+		
+		// char
+		char a = 'a';
+		
+		//string
+		
+		String a = "Sameer";
+		
+		
+		// true false
+		b
+			
+		boolean a = true;
+	}
+}
